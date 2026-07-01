47dd7b61d12e4b2ddf7e330ebd324050600f61d7
24BCAA-ARAVINDH <aravindhs1406@gmail.com>
2026-07-01 11:40:42 +0530

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,4 +1,21 @@
-/**
+/**const number=17;
+let isPrime=true;
+    if (num <= 1) {
+        isPrime= false;
+    }else{
+
+    for (let i = 2; i <=Math.sqrt(number); i++) {
+        if (num % i === 0) {
+            isPrime=false;
+            break;
+        }
+    }
+}
+if(isPrime){
+    console.log(number + " is a Prime Number");
+} else {
+    console.log(number + " is not a Prime Number");
+}
  * Checks if a number is a prime number.
  * @param {number} num - The number to check.
  * @returns {boolean} True if prime, false otherwise.
